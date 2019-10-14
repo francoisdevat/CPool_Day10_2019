406fe6d0ff4f9ceaf41f7417eb9efe20a7870f49
françois de vathaire <fvathaire@localhost.localdomain>
2019-10-14 09:49:01 -0400

    add of new task 1

--- lib/my/my_getnbr.c
+++ lib/my/my_getnbr.c
@@ -7,5 +7,26 @@
 
 int my_getnbr(char const *str)
 {
-    return (0);
+    int i;
+    int n;
+
+    i = 0;
+    n = 0;
+    if (str[0] == '-' && str[1] != '\0')
+        i++;
+    while (str[i] != '\0')
+    {
+        if ((str[i] < '0' || str[i] > '9'))
+            my_error("There must be only numbers in the string");
+            n = n + str[i] - 48;
+            n = n * 10;
+            i++;
+    }
+    n /= 10;
+    if (str[0] == '-') {
+        return (-1 * n);
+        else
+            return (n);
+
+    }
 }
